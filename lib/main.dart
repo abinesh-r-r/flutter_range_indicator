de2c9ddf12219b5739f8537f90439050a24249df
rrabinesh <138852428+rrabinesh@users.noreply.github.com>
2026-01-01 01:45:31 +0530

    Splash loading

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (_) => RangeProvider()..fetchRanges(),
+      create: (_) => RangeProvider(),
       child: MaterialApp(
         title: 'Range Indicator',
         debugShowCheckedModeBanner: false,
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/range_provider.dart';
 import 'range_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,16 +14,25 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _navigateToHome();
+    _initApp();
   }
 
-  _navigateToHome() async {
-    await Future.delayed(const Duration(seconds: 3));
-    if (!mounted) return;
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const RangeScreen()),
-    );
+  _initApp() async {
+    // Use Future.microtask to ensure context is available for Provider
+    Future.microtask(() async {
+      final provider = Provider.of<RangeProvider>(context, listen: false);
+      
+      await Future.wait([
+        Future.delayed(const Duration(seconds: 2)),
+        provider.fetchRanges(),
+      ]);
+
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const RangeScreen()),
+      );
+    });
   }
 
   @override
